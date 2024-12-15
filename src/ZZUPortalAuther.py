c81702c3f2174ffc6c720d76bbcb567235f42956
Illustar0 <me@illustar0.com>
2024-12-15 11:42:00 +0800

    style: reformat code

--- src/ZZUPortalAuther.py
+++ src/ZZUPortalAuther.py
@@ -1,14 +1,14 @@
 from zzupy import ZZUPy
 import toml
 
-config = toml.load('config.toml')
+config = toml.load("config.toml")
 try:
-    base_url = config['settings']['baseurl']
+    base_url = config["settings"]["baseurl"]
 except:
-    base_url = 'http://10.2.7.8:801'
-for name,data in config['accounts'].items():
-    usercode=data["usercode"]
+    base_url = "http://10.2.7.8:801"
+for name, data in config["accounts"].items():
+    usercode = data["usercode"]
     password = data["password"]
-    client=ZZUPy(usercode,password)
+    client = ZZUPy(usercode, password)
     for interface in data["interfaces"]:
-        print(client.Network.portal_auth(interface,base_url))
+        print(client.Network.portal_auth(interface, base_url))
